468453b2270440e411b944a6ca392c618602418c
gp2018hw <gp2018@hw.ac.uk>
2023-10-06 10:29:48 +0100

    Example 3 Implemented

---
+++ Week Four - Jquery/Example 3/Example 3.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("p").click(function(){
+      $(this).hide();
+    });
+  });
+
+  /* The above code hides all "p" (paragraphs) elements on screen, making them invisible to the user
+     viewing the webpage when the mouse button is clicked
+  */
