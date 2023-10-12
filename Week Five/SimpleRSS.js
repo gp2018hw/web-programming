4ff202c3fd3c535deda25a2bce769922ee5c6177
gp2018hw <gp2018@hw.ac.uk>
2023-10-12 10:09:19 +0100

    made working css file and tested

--- Week Five/SimpleRSS.js
+++ Week Five/SimpleRSS.js
@@ -7,22 +7,23 @@ async function XMLTest() {
     let parser = new DOMParser();
     let xmlDoc = parser.parseFromString(text, "text/xml")
 
-    console.log( xmlDoc );
+   // console.log( xmlDoc );
 
     const items = xmlDoc.querySelectorAll("item");
-    console.log('items:', items );
+   // console.log('items:', items );
     
     items.forEach( (el)=>{
         let title       = el.querySelector('title').textContent;
         let description = el.querySelector('description').textContent;
         let guid        = el.querySelector('guid').textContent;
     
-        console.log('title:',       title       );
-        console.log('description:', description);
-        console.log('guid:',        guid);
+   //     console.log('title:',       title       );
+   //     console.log('description:', description);
+   //     console.log('guid:',        guid);
     
         let div = document.createElement('div');
         document.body.appendChild( div );
+        div.classList.add("RSSBorder");
         div.innerHTML = `
             <h2>${title}</h2>
             <p>${description}</p>
